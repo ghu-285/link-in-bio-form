f8ac4096068bf62c630c0cc13a6156fb9d26b115
ghu-285 <151415214+ghu-285@users.noreply.github.com>
2025-02-16 22:21:59 +0000

    hm

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,19 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def new_item
+    render "item_templates/new_item"
+  end
+
+  def update_table
+    n = Item.new
+    n.link_url = params[:query_link_url]
+    n.link_description = params[:query_link_description]
+    n.thumbnail_url = params[:query_thumbnail_url]
+    n.save
+
+    redirect_to("/")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
+
+  get("/backdoor", {:controller => "items", :action => "new_item"})
+
+  post("/insert_item", {:controller => "items", :action => "update_table"})
 end
